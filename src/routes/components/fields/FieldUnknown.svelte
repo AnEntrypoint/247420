f12855901048924e817c304767330410c8316f1c
James Vos <almagestfraternite@gmail.com>
2024-03-09 17:17:30 +0000

    more changes

--- src/routes/components/fields/FieldUnknown.svelte
+++ src/routes/components/fields/FieldUnknown.svelte
@@ -1,14 +1,14 @@
 <script>
-    export let type;
-    export let name;
-    export let error;
-  </script>
-  
-  <div style="border: 1px dashed gray; padding: 8px;">
-    <p>
-      Field "{name}" unsupported: The type "{type}" has no UI component built yet.
-    </p>
-    {#if error}
-      <div id="{name}-error">{error}</div>
-    {/if}
-  </div>
+  export let type;
+  export let name;
+  export let error;
+</script>
+
+<div style="border: 1px dashed gray; padding: 8px;">
+  <p>
+    Field "{name}" unsupported: The type "{type}" has no UI component built yet.
+  </p>
+  {#if error}
+    <div id="{name}-error">{error}</div>
+  {/if}
+</div>
